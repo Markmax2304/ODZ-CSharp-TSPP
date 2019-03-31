810ab08343943afeb5aa836d831eb4204de92b39
Max <markmarks2304@gmail.com>
2019-03-31 16:44:16 +0300

    Added UI components

---
+++ ODZ_TSPP/ODZ_TSPP/Commands/ICommandButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODZ_TSPP.Commands
+{
+    public interface ICommandButton
+    {
+        void Execute();
+    }
+}
--- ODZ_TSPP/ODZ_TSPP/MainWindow.xaml.cs
+++ ODZ_TSPP/ODZ_TSPP/MainWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace ODZ_TSPP
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class View : Window
     {
-        public MainWindow()
+        public View()
         {
             InitializeComponent();
         }
